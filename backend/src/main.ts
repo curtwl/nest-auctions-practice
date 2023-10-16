2c5153aa9790089a08ec3d01d9d9bccc72c7e6f6
Curt <121463645+curtwl@users.noreply.github.com>
2023-10-16 01:08:10 -0500

    Enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   
-  await app.listen(3001);
+  app.enableCors();
+  await app.listen(8080);
 }
 bootstrap();
